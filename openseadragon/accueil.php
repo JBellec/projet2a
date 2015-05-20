78546b216d065a6ea4e018cb3380ef84a0f0cccb
Tumai <julien.bellec@hotmail.fr>
2015-05-20 11:45:29 +0200

    changement mot de passe BDD

--- openseadragon/accueil.php
+++ openseadragon/accueil.php
@@ -2,7 +2,7 @@
   <?php
    try
     {
-      $bdd = new PDO('mysql:host=localhost;dbname=projet2a;charset=utf8', 'root', '?jbtumai1992');
+      $bdd = new PDO('mysql:host=localhost;dbname=projet2a;charset=utf8', 'root', 'root');
     }
    catch (Exception $e)
    {
