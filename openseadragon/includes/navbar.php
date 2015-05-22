39a7c00340448b37dc9b9cfddbcaf1ac097a386e
Tumai <julien.bellec@hotmail.fr>
2015-05-22 14:13:45 +0200

    modif navbar, fonctionnement OK

--- openseadragon/includes/navbar.php
+++ openseadragon/includes/navbar.php
@@ -1,3 +1,4 @@
+   <?php include "header.php" ?>
     <div class="container">
         <!-- Static navbar -->
         <div class="navbar navbar-inverse" role="navigation">
@@ -27,6 +28,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
