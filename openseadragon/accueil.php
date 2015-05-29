282388611423b860306fdcc4135f5752ecfd20f7
daliadadie <dalia33@hotmail.fr>
2015-05-29 19:52:50 +0200

    acceuil.php modifié

--- openseadragon/accueil.php
+++ openseadragon/accueil.php
@@ -40,66 +40,67 @@
      ?>
          
     <div class="container">
-      <div class="jumbotron">
-        <h3>Plateforme Web de Pathologie Numérique</h3><br/>
-        <h4>****Recherche avancée**** </h4>
-          <form class="form-inline" method ="get" action ="accueil.php">
-            <label>Barcode : </label>
-            <input name ="barcode" type ="text" placeholder ="TCGA-**-****" size="13"/>&emsp;
-            <label>Status vital : </label>
-            <select name="status">
-                <option value=""></option>
-                <option value="Dead">Dead</option>
-                <option value="Alive">Alive</option>
-            </select>&emsp;
-            <label>Genre : </label>
-            <select name="gender">
-                <option value=""></option>
-                <option value="MALE">male</option>
-                <option value="FEMALE">female</option>
-            </select>&emsp;&emsp;&emsp;&emsp;&emsp;
-            <button type="submit" class="btn btn-default">Search</button>
-          </form>
-      </div>
+      	<div class="jumbotron">
+        		<h3>Plateforme Web de Pathologie Numérique</h3><br/>
+        		<h4>****Recherche avancée**** </h4>
+	          <form class="form-inline" method ="get" action ="accueil.php">
+	            <label>Barcode : </label>
+	            <input name ="barcode" type ="text" placeholder ="TCGA-**-****" size="13"/>&emsp;
+	            <label>Status vital : </label>
+	           		 <select name="status">
+		  		 <option value=""></option>
+		  		 <option value="Dead">Dead</option>
+		  		<option value="Alive">Alive</option>
+	            		</select>&emsp;
+	            <label>Genre : </label>
+	            		<select name="gender">
+		  		 <option value=""></option>
+		   		<option value="MALE">male</option>
+		  		 <option value="FEMALE">female</option>
+	           		 </select>&emsp;&emsp;&emsp;&emsp;&emsp;
+	            <button type="submit" class="btn btn-default">Search</button>
+	          </form>
+      	</div>
           
           <div class="row">
-            <?php
-             while($image = $req->fetch()){ 
-            ?> 
-            <div class="col-xs-2 col-md-2">
-              <h2 class="titre"><?php echo $image['barcode'];?></h2>
-              <a href="dzi_img.php?titre=<?php echo $image['titre'];?>" ><img src='<?php echo $image['chemin'];?>' /></a>
-              <li class="dropdown">
-                <a class="btn btn-default" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">View details <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                  <li>Gender : <?php echo $image['genre'];?></li>
-                  <li>Status : 
-                    <?php if (strcmp($image['vital_status'],'Dead')==0){ ?>
-                              <span class="label label-danger">Dead</span><br/>
-                    <?php }else{ ?>
-                            <span class="label label-success">Alive</span><br/>
-                    <?php } ?></li>
-                     <li>Race : <?php echo $image['race'];?></li>
-                     <li>Country : <?php echo $image['pays'];?></li>
-                    <li>Tumor : <?php echo $image['tumeur'];?></li>
-                    <li>Days to birth : <?php echo $image['daysToBirth'];?></li>
-                    <li>Days to death : <?php echo $image['daysToDeath'];?></li>
-                    <li>Tumor status : <?php echo $image['tumor_status'];?></li>
-                    <li>Days to last followup : <?php echo $image['last_contact'];?></li>
-                    <li>Age at diagnosis : <?php echo $image['age_diagnosis'];?></li>
-                    <li>Er status by IHC : <?php echo $image['er_status'];?></li>
-                    <li>Pr status by IHC : <?php echo $image['pr_status'];?></li>
-                    <li>Her2 status by IHC : <?php echo $image['her2_status'];?></li>
-                    <li>Histological type : <?php echo $image['histological_type'];?></li>
-                </ul>
-              </li>
-            </div>
-              <?php
-                $i = ($i+1)%6;
-                if($i==6) {
-              ?>
+         			   <?php
+         			  while($image = $req->fetch()){ 
+          			  ?> 
+		            <div class="col-sm-2 col-md-2">
+			 	<h2 class="titre"><?php echo $image['barcode'];?></h2>
+			 	<a href="dzi_img.php?titre=<?php echo $image['titre'];?>" ><img src='<?php echo $image['chemin'];?>' /></a>
+			 	<li class="dropdown">
+			   	<a class="btn btn-default" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">View details <span class="caret"></span></a>
+			   	<ul class="dropdown-menu" role="menu">
+			     	      <li>Gender : <?php echo $image['genre'];?></li>
+			    	      <li>Status : 
+			       		<?php if (strcmp($image['vital_status'],'Dead')==0){ ?>
+				  		  <span class="label label-danger">Dead</span><br/>
+			       		<?php }else{ ?>
+					  <span class="label label-success">Alive</span><br/>
+			       		<?php } ?>
+				        </li>
+				        <li>Race : <?php echo $image['race'];?></li>
+				        <li>Country : <?php echo $image['pays'];?></li>
+				       <li>Tumor : <?php echo $image['tumeur'];?></li>
+				       <li>Days to birth : <?php echo $image['daysToBirth'];?></li>
+				       <li>Days to death : <?php echo $image['daysToDeath'];?></li>
+				       <li>Tumor status : <?php echo $image['tumor_status'];?></li>
+				       <li>Days to last followup : <?php echo $image['last_contact'];?></li>
+				       <li>Age at diagnosis : <?php echo $image['age_diagnosis'];?></li>
+				       <li>Er status by IHC : <?php echo $image['er_status'];?></li>
+				       <li>Pr status by IHC : <?php echo $image['pr_status'];?></li>
+				       <li>Her2 status by IHC : <?php echo $image['her2_status'];?></li>
+				       <li>Histological type : <?php echo $image['histological_type'];?></li>
+			   	</ul>
+				</li>
+		            </div>
+        			 <?php
+             		  $i = ($i+1)%6;
+               		 if($i==6) {
+            			  ?>
                   </div>
-                  </div>
+ 
           <?php
                  } }
              
